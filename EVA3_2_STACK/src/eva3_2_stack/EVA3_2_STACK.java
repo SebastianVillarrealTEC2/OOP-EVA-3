e50f12cd7481bdf68104a374fcbd19b701faf6a0
SebastianVillarrealTEC2 <L25550588@chihuahua2.tecnm.mx>
2026-04-29 21:51:51 -0600

    Add files via upload

---
+++ EVA3_2_STACK/src/eva3_2_stack/EVA3_2_STACK.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva3_2_stack;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_2_STACK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A();
+    }
+    public static void A(){
+        B();
+    }
+    public static void B(){
+        C();
+    }
+    public static void C(){
+        int x=100, y =0, resu;
+        resu= x/y ;
+        System.out.println("resu" +  resu);
+    }
+}
